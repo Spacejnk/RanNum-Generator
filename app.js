266c1a8d59cec3b6069e9bcfd73202442cb32e51
Timits <will4f21@yahoo.com>
2021-01-24 05:09:16 -0800

    create num grid/ click func innerhtml

---
+++ app.js
@@ -0,0 +1,7 @@
+let generator = document.getElementById('grid-container');
+let genInfo = document.getElementById('gen-info')
+
+function gridGen() {
+    console.log('click!');
+    genInfo.innerHTML = "NumGen!";
+}
